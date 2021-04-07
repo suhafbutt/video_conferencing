877b52c099e427c4099d7ebb0fd7de65dccde6fd
Suhaf Butt <suhaf.azam@gmail.com>
2021-04-07 11:48:47 +0200

    minor refactoring

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,4 @@
 
 Mentor.create([{first_name: 'Max', last_name: 'Mustermann', email: 'max@xyz.com', time_zone: 'Pacific Time (US & Canada)'}])
 Student.create([{first_name: 'Joe', last_name: 'Hart', email: 'joe@xyz.com', time_zone: 'Berlin'}])
-Mentor.create([{first_name: 'Sergio', last_name: 'Ramos', email: 'sergio@xyz.com', time_zone: 'Budapest'}])
 Student.create([{first_name: 'Ryan', last_name: 'Harris', email: 'ryan@abc.com', time_zone: 'Dublin'}])
