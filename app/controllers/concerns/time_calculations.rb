9103048d30187e8845b26ef525063a5cb949e4f4
Suhaf Butt <suhaf.azam@gmail.com>
2021-04-28 04:22:13 +0200

    tweak

--- app/controllers/concerns/time_calculations.rb
+++ app/controllers/concerns/time_calculations.rb
@@ -11,10 +11,19 @@ module TimeCalculations
         already_booked = CareerFoundryApi.get_mentor_agenda(date)
         parsed_date = date.to_datetime
         appointment_slots = @mentor.appointments.booked(date).pluck(:starts_at)
-        complete_list_of_hours = already_booked.map { |e| [e['date_time'], 'Booked'] }
-        return complete_list_of_hours + (parsed_date.to_i .. (parsed_date.end_of_day).to_i).step(1.hour).map { |e|
-                  [Time.at(e), appointment_slots.include?(Time.at(e)) ? 'Booked' : 'Available']
-                }
+        complete_list_of_hours_api = already_booked.map { |e| [e['date_time'], 'Booked'] }
+        return complete_list_of_hours_api + complete_list_of_hours_db(parsed_date, appointment_slots)
       end
+    end
+
+    #
+    # Returns list of all time slots with status of the given date
+    # @param {DateTime} parsed_date
+    # @param {Appointment} appointment_slots
+    #
+    def complete_list_of_hours_db(parsed_date, appointment_slots)
+      return (parsed_date.to_i .. (parsed_date.end_of_day).to_i).step(1.hour).map { |e|
+                [Time.at(e), appointment_slots.include?(Time.at(e)) ? 'Booked' : 'Available']
+              }
     end
 end
